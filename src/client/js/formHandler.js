d1ec7c2233b27cbfb56a9521fed73b3fed798302
Kelly Cho <wenhsiu55@gmail.com>
2019-10-31 22:31:35 +0800

    Buggy post function.

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,47 @@
 function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkForName(formText)
+    let input = document.getElementById('url').value;
+    Client.checkForName(input);
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+    console.log("::: Form Submitted :::");
+
+    postData('/post', {url: input, test: 'ok'});
+
+    /*fetch('http://localhost:8080/')
+
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+        document.getElementById('results').innerHTML = res.polarity + " " + res.subjectivity
+    })*/
 }
 
-export { handleSubmit }
+// async POST function
+const postData = async(url = '', data = {}) => {
+
+    const response = await fetch (url, {
+    method: 'POST', // * GET, POST, PUT, DELETE, etc.
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+        'Content-Type': 'application/json', // indicates that our app run on JSON data
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+
+    try {
+        const newData = await response.json();
+        return newData;
+
+    } catch(error) {
+        console.log('error', error);
+    }
+}
+
+
+
+
+export { 
+    handleSubmit,
+    postData 
+}
